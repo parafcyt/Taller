51db279c4e1f8dc38227315af90b8abe0ec1f768
Alexis Morison <alexismorison83@gmail.com>
2022-09-29 19:42:04 -0300

    agrega repositorio para destinos

---
+++ backend/Infraestructure/Repositories/DestinationRepository.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Entities;
+using Domain.Repositories;
+using Infraestructure.DataContext;
+using Infraestructure.Repositories.Base;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infraestructure.Repositories
+{
+    public class DestinationRepository : Repository<Destination>, IDestinationRepository
+    {
+        public DestinationRepository(TallerContext pTallerContext) : base(pTallerContext)
+        {
+        }
+    }
+}
