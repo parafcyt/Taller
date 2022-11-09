01739dbdac499f52c01767265bcbbb180373c9de
Alexis Morison <alexismorison83@gmail.com>
2022-11-09 20:53:51 -0300

    genera routing basico a los modulos

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./modules/client/client.module')
+      .then(mod => mod.ClientModule)
+  },
+  {
+    path: 'administration',
+    loadChildren: () => import('./modules/admin/admin.module')
+      .then(mod => mod.AdminModule)
+  },
+  {
+    path: 'account',
+    loadChildren: () => import('./modules/account/account.module')
+      .then(mod => mod.AccountModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/modules/client/client-routing.module.ts
+++ frontend/src/app/modules/client/client-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ClientComponent } from './client.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ClientComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
