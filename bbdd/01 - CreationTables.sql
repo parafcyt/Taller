08e4589e7e8b6de501aaccf246219e4d25c72de0
Beatriz Vazquez <45252114+parafcyt@users.noreply.github.com>
2023-03-05 20:09:21 -0300

    Actualiza diagrama, actualiza scripts de creación de tablas y crea script de inserción de datos

--- bbdd/01 - CreationTables.sql
+++ bbdd/01 - CreationTables.sql
@@ -1,22 +1,35 @@
 CREATE TABLE [User] (
-	[UserId] VARCHAR(500) PRIMARY KEY,
-	[State] BIT NOT NULL,
-	[Name] VARCHAR(100) NOT NULL,
-	[Email] VARCHAR(100)
+	[UserId] INT PRIMARY KEY IDENTITY(1,1),
+	[LoginName] VARCHAR(100) NOT NULL,
+	[Password] VARCHAR(MAX) NOT NULL,
+	[Active] BIT NOT NULL,
+	[Name] VARCHAR(100),
+	[LastName] VARCHAR(100),
+	[Email] VARCHAR(100),
+	[CreatedDate] DATETIME NOT NULL
+);
+
+CREATE TABLE [RegistrationMethod] (
+	[RegistrationMethodId] TINYINT PRIMARY KEY IDENTITY(1,1),
+	[Name] VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE [Customer] (
-	[CustomerId] VARCHAR(500) PRIMARY KEY,
-	[Name] VARCHAR(100) NOT NULL,
-	[LastName] VARCHAR(100) NOT NULL,
-	[DNI] VARCHAR(50) NOT NULL,
-	[Email] VARCHAR(100) NOT NULL,
-	[Birthdate] DATE NOT NULL,
-	[PhoneNumber] VARCHAR(100) NOT NULL
+	[CustomerId] INT PRIMARY KEY IDENTITY(1,1),
+	[CustomerSourceId] VARCHAR(500) NOT NULL,
+	[Name] VARCHAR(100),
+	[LastName] VARCHAR(100),
+	[DNI] VARCHAR(50),
+	[Email] VARCHAR(100),
+	[Birthdate] DATE,
+	[PhoneNumber] VARCHAR(100),
+	[RegistrationCompleted] BIT NOT NULL DEFAULT(0),
+	[CreatedDate] DATETIME NOT NULL,
+	[RegistrationMethodId] TINYINT NOT NULL REFERENCES [RegistrationMethod]([RegistrationMethodId])
 );
 
 CREATE TABLE [RatingScale] (
-	[RatingScaleId] TINYINT PRIMARY KEY IDENTITY(1,1),
+	[RatingScaleId] TINYINT PRIMARY KEY IDENTITY(0,1),
 	[Name] VARCHAR(350) NOT NULL
 );
 
@@ -72,13 +85,13 @@ CREATE TABLE [Excursion] (
 CREATE TABLE [Product] (
 	[ProductId] INT PRIMARY KEY IDENTITY(1,1),
 	[Name] VARCHAR(350) NOT NULL,
-	[Description] VARCHAR(500),
 	[DepartureDate] DATE NOT NULL,
 	[ReturnDate] DATE NOT NULL,
-	[Price] FLOAT NOT NULL,
-	[Stock] INT NOT NULL,
+	[Price] FLOAT NOT NULL CHECK(Price >= 0),
+	[Stock] INT NOT NULL CHECK(Stock >= 0),
 	[DocumentationDes] VARCHAR(350),
-	[State] BIT NOT NULL,
+	[Active] BIT NOT NULL DEFAULT(0),
+	[CreatedDate] DATETIME NOT NULL,
 	[BookingTypeId] TINYINT NOT NULL REFERENCES [BookingType]([BookingTypeId]),
 	[OriginId] INT NOT NULL REFERENCES [Origin]([OriginId]),
 	[DestinationId] INT NOT NULL REFERENCES [Destination]([DestinationId]),
@@ -87,7 +100,7 @@ CREATE TABLE [Product] (
 
 CREATE TABLE [ProductTransport] (
 	[ProductTransportId] INT PRIMARY KEY IDENTITY(1,1),
-	[Order] TINYINT NOT NULL,
+	[Order] TINYINT NOT NULL CHECK([Order] >= 0),
 	[Information] VARCHAR(500),
 	[ProductId] INT NOT NULL REFERENCES [Product]([ProductId]),
 	[TransportId] INT NOT NULL REFERENCES [Transport]([TransportId])
@@ -101,27 +114,48 @@ CREATE TABLE [ProductExcursion] (
 
 CREATE TABLE [Offer] (
 	[OfferId] INT PRIMARY KEY IDENTITY(1,1),
-	[OfferPrice] FLOAT NOT NULL,
-	[CreationDate] DATE NOT NULL,
-	[State] BIT NOT NULL,
+	[OfferPrice] FLOAT NOT NULL CHECK(OfferPrice >= 0),
+	[StartDate] DATETIME NOT NULL,
+	[EndtDate] DATETIME NOT NULL,
+	[Active] BIT NOT NULL,
 	[BannerText] VARCHAR(100),
+	[CreatedtDate] DATETIME NOT NULL,
 	[ProductId] INT NOT NULL REFERENCES [Product]([ProductId])
 );
 
 CREATE TABLE [Favorite] (
 	[FavoriteId] INT PRIMARY KEY IDENTITY(1,1),
-	[ProductId] INT REFERENCES [Product]([ProductId]),
-	[CustomerId] VARCHAR(500) NOT NULL REFERENCES [Customer]([CustomerId]),
+	[ProductId] INT NOT NULL REFERENCES [Product]([ProductId]),
+	[CustomerId] INT NOT NULL REFERENCES [Customer]([CustomerId]),
+	[CreatedtDate] DATETIME NOT NULL
+);
+
+CREATE TABLE [OrderState] (
+	[OrderStateId] TINYINT PRIMARY KEY IDENTITY(1,1),
+	[Name] VARCHAR(350) NOT NULL
+);
+
+CREATE TABLE [Payment] (
+	[PaymentId] INT PRIMARY KEY IDENTITY(1,1),
+	[SourceId] INT NOT NULL,
+	[CreatedtDate] DATETIME NOT NULL,
+	[ApprovedDate] DATETIME NOT NULL,
+	[PaymentMethodId] VARCHAR(100) NOT NULL,
+	[PaymentTypeId] VARCHAR(100) NOT NULL,
+	[Status] VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE [Order] (
 	[OrderId] INT PRIMARY KEY IDENTITY(1,1),
-	[Quantity] INT NOT NULL,
-	[UnitPricePaid] FLOAT NOT NULL,
-	[Total] FLOAT NOT NULL,
-	[DateOrder] DATE NOT NULL,
-	[State] BIT NOT NULL DEFAULT(1),
+	[Quantity] INT NOT NULL CHECK(Quantity > 0),
+	[UnitPrice] FLOAT NOT NULL CHECK(UnitPrice >= 0),
+	[TotalPrice] FLOAT NOT NULL CHECK(TotalPrice >= 0),
+	[Discount] FLOAT,
+	[CreatedDate] DATETIME NOT NULL,
+	[OrderStateId] TINYINT NOT NULL REFERENCES [OrderState]([OrderStateId]),
 	[ProductId] INT NOT NULL REFERENCES [Product]([ProductId]),
-	[CustomerId] VARCHAR(500) NOT NULL REFERENCES [Customer]([CustomerId]),
-	[RatingScaleId] TINYINT NULL REFERENCES [RatingScale]([RatingScaleId])
+	[CustomerId] INT NOT NULL REFERENCES [Customer]([CustomerId]),
+	[RatingScaleId] TINYINT NOT NULL REFERENCES [RatingScale]([RatingScaleId]) DEFAULT(0),
+	[PaymentId] INT NOT NULL REFERENCES [Payment]([PaymentId])
 );
+
---
+++ bbdd/02 - InsertData.sql
@@ -0,0 +1,33 @@
+INSERT INTO [dbo].[RatingScale]
+			([Name])
+VALUES
+    ('Sin valorar'),
+	('Malo'),
+	('Regular'),
+	('Bueno'),
+	('Muy bueno'),
+	('Excelente')
+;
+
+INSERT INTO [dbo].[OrderState]
+           ([Name])
+VALUES
+ 	('En espera'),
+	('En curso'),
+	('Completado')
+;
+
+
+INSERT INTO [dbo].[BookingType]
+           ([Name])
+     VALUES
+ 		('No aplica'),
+		('Media pensión'),
+		('All inclusive')
+;
+
+
+
+
+
+
